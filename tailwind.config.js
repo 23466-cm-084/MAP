cba539ec6be89d06fc52e07fe93a028a0d82952b
SYED BILAL <syed8688750645@gmail.com>
2025-10-13 18:24:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["*.html"],
+      theme: {
+        extend: {
+          screens: {
+            'xs': '375px', // Custom breakpoint for 375px
+            // You can keep or modify the default breakpoints if needed
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+          },
+        },
+      },
+  plugins: [],
+}
+
