94a5cb61aa8b3b7d07f6ac5913828f965a55ed84
SYED BILAL <syed8688750645@gmail.com>
2025-10-22 20:36:33 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         extend: {
           screens: {
             'xs': '375px', // Custom breakpoint for 375px
+            'sx': '430px', // Custom breakpoint for 430px
             // You can keep or modify the default breakpoints if needed
             'sm': '640px',
             'md': '768px',
